188cfdea204f2dd42f43014b3f302f0cdb77035d
npero <npero@cirb.irisnet.be>
2011-12-13 13:15:13 +0100

    Fixed deployment files.

--- prod.sh
+++ prod.sh
@@ -3,17 +3,12 @@
 use feature qw(say);
 use Term::ReadKey;
 use FindBin;
-use Getopt::Long;
 
-$major_flag='';
-GetOptions ("major"  => \$major_flag);
 ;
 
 $release_tag_regex=qr/(\w+)-(\d)\.(\d+)\.(\d+)\.?(\d*)/;
-$VERSION = '$major.$minor.$release.$bugfix';
-$NEW_VERSION = '$new_major.$new_minor.$release.$bugfix';
+$VERSION = '$major.$minor.$hotfix';
 $TAG = '$env_name' . '-' . $VERSION;
-$NEW_TAG = '$env_name' . '-' . $NEW_VERSION;
 
 # Pass a CONSTANT string containing $variable to interpolate dynamically.
 sub interpolate {
@@ -28,8 +23,8 @@ sub restore_and_die {
 $working_dir = "$FindBin::RealBin/../../";
 # chdir($working_dir);
 
-$branch_name = "staging";
-$uri="http://192.168.13.78:8080";
+$branch_name = "prod";
+$uri="http://192.168.15.173:8080";
 
 $fix_release = 1 if $ARGV[1] eq "FIX";
 $roll_release = 1 if $ARGV[1] eq "ROLL";
@@ -50,57 +45,42 @@ $diff=`git diff --stat origin/$branch_name $branch_name`; restore_and_die "Pleas
 
 if ($roll_release) {
     say "Roll release: check if master is in sync with Github.";
-	`git fetch origin master`;  restore_and_die "Cannot fetch origin/master, stopped" if $?;
-	$diff=`git diff --stat origin/master master`; restore_and_die "Please synchronize your master before any deployment, stopped" if $diff;
+	`git fetch origin staging`;  restore_and_die "Cannot fetch origin/staging, stopped" if $?;
+	$diff=`git diff --stat origin/staging staging`; restore_and_die "Please synchronize your staging before any deployment, stopped" if $diff;
 }
 
-$_ = `git describe --tags --abbrev=0 --match=prod-*`;
-print "Current prod tag is $_";
-if (/$release_tag_regex/) {
-	$new_major = $2;
-	$new_minor = $3;
-	if($major_flag){
-		$new_major ++;
-		$new_minor = '0';
-	}else{
-		$new_minor ++;
-	}
-} else {
-	restore_and_die "Wrong prod tag description, stopped";
-}
 
-$_ = `git describe --tags --abbrev=0 --match=staging-*`; 
+$_ = `git describe --tags --abbrev=0 --match=prod-*`; 
 print "Current staging tag is $_";
 if (/$release_tag_regex/) {
+	$env_name = $1;
 	$major = $2;
 	$minor = $3;
-	$env_name = $1;
-   	$release = $4;
-	$bugfix = $5;
+   	$hotfix = $4;
 } else {
 	restore_and_die "Wrong tag description, stopped";
 }
 
 $current_version = interpolate $VERSION;
-$bugfix_branchname = "bugfix-$current_version";
+$hotfix_branchname = "hotfix-$current_version";
 
 if($roll_release) {
-	$release ++;
-	$bugfix = '0';
-	$new_tag = interpolate $NEW_TAG;
-	$new_version= interpolate $NEW_VERSION;
+	$minor ++;
+	$hotfix = '0';
+	$new_tag = interpolate $TAG;
+	$new_version= interpolate $VERSION;
 	# We would like a linear staging/prod branch without the feature commits
-	`git merge --no-ff master -m "$new_tag"`; restore_and_die "Cannot merge master into $branch_name, stopped" if $?;
-	say "Merge master into $branch_name, done.";
-	say "Sub-version tag has been incremented and the bug count reset.";
+	`git merge --no-ff staging -m "$new_tag"`; restore_and_die "Cannot merge master into $branch_name, stopped" if $?;
+	say "Merge staging into $branch_name, done.";
+	say "Minor tag has been incremented and the bug count reset.";
    	say qq/Start deployment of the rolling release "$new_tag"./;
 } elsif ($fix_release) {
-	$bugfix ++;
-	$new_tag = interpolate $NEW_TAG;
-	$new_version = interpolate $NEW_VERSION;
+	$hotfix ++;
+	$new_tag = interpolate $TAG;
+	$new_version = interpolate $VERSION;
 
-	`git merge --ff-only $bugfix_branchname`; restore_and_die "Cannot merge bugfix into $branch_name, stopped" if $?;
-   	say "Merge $bugfix_branchname into $branch_name, done.";
+	`git merge --ff-only $hotfix_branchname`; restore_and_die "Cannot merge bugfix into $branch_name, stopped" if $?;
+   	say "Merge $hotfix_branchname into $branch_name, done.";
 
    	say qq/Start deployment of the fixing release "$new_tag"/;
 }
@@ -128,20 +108,22 @@ say "Publish_$branch_name into Github";
 
 say "Deployment successful.";
 
+`git branch -D $hotfix_branchname`; restore_and_die "Cannot delete previous bugfix-branch : $bugfix_branchname" if $?;
+`git push origin :$hotfix_branchname`; restore_and_die "Cannot delete previous remote bugfix-branch : origin/$bugfix_branchname" if $?;
 
-`git branch -D $bugfix_branchname`; restore_and_die "Cannot delete previous bugfix-branch : $bugfix_branchname" if $?;
-`git push origin :$bugfix_branchname`; restore_and_die "Cannot delete previous remote bugfix-branch : origin/$bugfix_branchname" if $?;
+$new_hotfix_branchname = "hotfix-$new_version";
+`git branch $new_hotfix_branchname`;
+`git push origin $new_hotfix_branchname`;
 
-$new_bugfix_branchname = "bugfix-$new_version";
-`git branch $new_bugfix_branchname`;
-`git push origin $new_bugfix_branchname`;
-
-`git checkout master`; die "Cannot checkout master, stopped" if $?;
+`git checkout staging`; die "Cannot checkout staging, stopped" if $?;
 if($fix_release){
 	`git cherry-pick $new_tag`; restore_and_die "Cannot cherry-pick $new_tag, stopped" if $?;
 	`git commit --amend -m "$new_tag"`; restore_and_die "Cannot amend the last commit after cherry-picking" if $?;
-	`git push origin master`; restore_and_die "Cannot push master to origin, stopped" if $?;
+	`git push origin staging`; restore_and_die "Cannot push staging to origin, stopped" if $?;
 }
+
+`git checkout master`; die "Cannot checkout master, stopped" if $?;
+
 ;
 # say "Start code analysing with sonar";
 # `mvn sonar:sonar  -DartifactVersion=$new_version -Dsonar.dynamicAnalysis=reuseReports -Dsonar.skipDesign=true`;
